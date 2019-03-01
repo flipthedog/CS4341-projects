0d7dd77422d8e8341b825078bd476805ca1cb12a
kyrab <38116068+krbrez@users.noreply.github.com>
2019-03-01 14:22:32 -0500

    Getting there with b avoidance Still need to get bomb avoidance in algos

--- Bomberman/group05/Scenario2Characters/finitestatecharacterfinalS2.py
+++ Bomberman/group05/Scenario2Characters/finitestatecharacterfinalS2.py
@@ -116,10 +116,10 @@ class FiniteStateCharacter(CharacterEntity):
                         return True
                 return False
 
-    def expectimax(self, wrld, exit, meX, meY, TickForwards = True):
+    def expectimax(self, wrld, exit, meX, meY, TickForwards = True, badPositions, timeLeft):
         # Complete the greedy algorithm
         # Get the [x,y] coords of the next cell to go to
-        goTo = EM.expectiMax(wrld, exit, 2, TickForwards)
+        goTo = EM.expectiMax(wrld, exit, 2, TickForwards, badPositions, timeLeft)
         if  goTo[0] == exit[0] and goTo[1] == exit[1]:
             raise ValueError
         # move in direction to get to x,y found in prev step
@@ -148,7 +148,7 @@ class FiniteStateCharacter(CharacterEntity):
             #move in direction to get to x,y found in prev step
             self.move(-meX + goTo[0], -meY + goTo[1])
 
-    def bombAvoidanceNoMster(self, wrld, meX, meY):
+    def bombAvoidance(self, wrld, meX, meY):
         # Get the bomb object
         thisBomb = next(iter(wrld.bombs.values()))[0]
 
@@ -159,24 +159,76 @@ class FiniteStateCharacter(CharacterEntity):
         # Figure out how far away from the explosion you are using euclidean distance
         manhattan = meX-thisBomb.x+ meY-thisBomb.y
         # Figure if on the same x or y as the bomb
-        horz = False
-        if bomb.x == meX:
-            horz = True
         vert = False
-        if bomb.y = meY:
+        if bomb.x == meX:
             vert = True
-        if manhattan <= bRange and straightShot:
+        horz = False
+        if bomb.y = meY:
+            horz = True
+        if meX == bomb.x and meY == bomb.y:
+            # Use the movement algorithms but send in the positions that the bomb will explode in so that the algorithm
+            # knows to avoid those spaces
+            badPositions = []
+            badPositions.append([bomb.x,bomb.y])
+            for i in range(bRange):
+                badPositions.append([bomb.x+1+i,bomb.y])
+                badPositions.append([bomb.x-1-i,bomb.y])
+                badPositions.append([bomb.x,bomb.y+1+i])
+                badPositions.append([bomb.x,bomb.y-1-i])
+            # Then send to expectimax or greedy/astar
+            if self.isThereMonster(wrld, meX, meY):
+                self.expectimax(wrld, exit, meX, meY, False, badPositions, timeLeft)
+            else:
+                self.greedy(wrld, exit, meX, meY, False, badPositions, timeLeft)
+        elif manhattan <= bRange and (horz or vert):
             # then you are still within the range of the bomb
-
+            #TODO: Add in the intitial run away case
             # Figure out how much further you'd have to move to be out of the range of the bomb
             howMuchFurther = bRange - euclid
             if timeLeft <= howMuchFurther:
                 # if you can't escape it in time by going straight, see if you can go diagonally
-                #TODO: fill in
-                pass
+                if horz:
+                    # if the character is in the horizontal direction from the bomb, try going up
+                    # or down to escape, or just accept your fate if not
+                    if not wrld.wall_at(meX, meY + 1) and not wrld.monster_at(meX, meY + 1):
+                        self.move(0, 1)
+                    elif not wrld.wall_at(meX, meY - 1) and not wrld.monster_at(meX, meY - 1):
+                        self.move(0, -1)
+                    else:
+                        self.move(0, 0)
+                else:
+                    # if the character is in the vertical direction from the bomb, try going left
+                    # or right to escape, or just accept your fate if not
+                    if not wrld.wall_at(meX + 1, meY) and not wrld.monster_at(meX + 1, meY):
+                        self.move(1, 0)
+                    elif not wlrd.wall_at(meX - 1, meY) and not wrld.monster_at(meX - 1, meY):
+                        self.move(-1, 0)
+                    else:
+                        self.move(0, 0)
             else:
                 # continue running away in the same direction
-                #TODO: fill in
-                pass
+                if horz:
+                    if meX < bomb.x:
+                        self.move(-1, 0)
+                    else:
+                        self.move(1, 0)
+                else:
+                    if meY < bomb.y:
+                        self.move(0, -1)
+                    else:
+                        self.move(0, 1)
         else:
-            # Don't move into the path 
+            # Use the movement algorithms but send in the positions that the bomb will explode in so that the algorithm
+            # knows to avoid those spaces
+            badPositions = []
+            badPositions.append([bomb.x,bomb.y])
+            for i in range(bRange):
+                badPositions.append([bomb.x+1+i,bomb.y])
+                badPositions.append([bomb.x-1-i,bomb.y])
+                badPositions.append([bomb.x,bomb.y+1+i])
+                badPositions.append([bomb.x,bomb.y-1-i])
+            # Then send to expectimax or greedy/astar
+            if self.isThereMonster(wrld, meX, meY):
+                self.expectimax(wrld, exit, meX, meY, False, badPositions, timeLeft)
+            else:
+                self.greedy(wrld, exit, meX, meY, False, badPositions, timeLeft)
